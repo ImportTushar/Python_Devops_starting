3abca1de2eca449ec3c689c27656c66679a7776d
ImportTushar <Tushargiri9901@gmail.com>
2024-05-06 16:56:36 +0530

    is Python Learning branch

---
+++ Devops/Data Types.py
@@ -0,0 +1,41 @@
+x=1;
+print(type(x))
+
+y=5.2
+print(type(y))
+
+z='Hello World'
+print(type(z))
+
+a= 1j+5
+print(type(a))
+
+b = True
+print(type(b))
+
+c=[1,2,3,4,5]
+print(type(c))
+
+d=(1,2,3,4,5)
+print(type(d))
+
+e={'name':'Tushar','age':50}
+print(type(e))
+
+f = b'Tushar'
+print(type(f))
+
+g = bytearray(6)
+print(type(g))
+
+h=memoryview(bytes(6))
+print(type(h))
+
+I = {1,2,3,4,5,6}
+print(type(I))
+
+J = frozenset({1,2,3,4,5,6})
+print(type(J))
+
+k = None
+print(type(k))
