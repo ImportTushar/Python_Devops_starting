e108938937845a5df3a2a89fdf9a22cbc8c9b71b
ImportTushar <tushargiri9901@gmail.com>
2024-05-07 16:03:50 +0530

    Advance Data Type

---
+++ Devops/Data_Types_advance.py
@@ -0,0 +1,15 @@
+num = 3e6
+print(num)
+
+print(abs(-5))
+
+print(pow(2,3))
+
+print(round(4.9))
+
+num = [1,2,3,4,5,6]
+print(min(num))
+
+print(max(num))
+
+print(sum(num))
---
+++ Devops/Sequence_String_data_type.py
@@ -0,0 +1,14 @@
+name = 'TUSHAR GIRI'
+print(name[4])
+
+print(name[5])
+
+print(name[2:6])
+
+# Contacination
+x='Tushar'
+y='Giri'
+z=(x+' '+y)
+print(z)
+
+
